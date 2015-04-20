8471748c99d6d59a5de1e726c8fa52c34ace719c
thinkholic <mailtokasun@gmail.com>
2015-04-20 18:06:44 +0530

    code standards

--- client/components/editor.js
+++ client/components/editor.js
@@ -2,11 +2,8 @@
 Template['editor'].events({
   "submit form": function(e) {
     e.preventDefault();
-
     chatMsg = document.getElementById("chat-msg").value;
-
     FlowComponents.callAction("onSubmit", chatMsg);
-
     document.getElementById("chat-msg").value = '';
   }
 });
