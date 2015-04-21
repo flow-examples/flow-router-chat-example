ef98da4f2162312806beb9afd791ab869db01579
thinkholic <mailtokasun@gmail.com>
2015-04-21 17:04:12 +0530

    Style changes + rid HTML Strings on component.js

--- client/components/component.js
+++ client/components/component.js
@@ -1,25 +1,13 @@
 // statcPageComponent
 var statcPageComponent = FlowComponents.define('page',function(props) {
-	this.set('pageName',props.name)
-	this.setPageContent();
 });
 
-statcPageComponent.prototype.setPageContent = function(){
-	samplePageTitle = '<h2>Page Title</h2>';
-	samplePageContent = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur diam sed ipsum hendrerit finibus. Aenean purus leo, tristique at erat a, viverra consequat lacus. Nulla imperdiet convallis dapibus. Vivamus blandit at sapien at feugiat. Morbi volutpat pellentesque ligula. Vestibulum ac iaculis orci. Nunc tincidunt augue eget metus porta, ac vehicula ipsum fermentum. Phasellus ut pellentesque libero, id dapibus augue. Suspendisse id porta velit. Quisque aliquet faucibus pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>';
-	samplePageContent += '<p>Sed vitae ante accumsan, consequat velit id, posuere libero. Phasellus enim arcu, fermentum hendrerit tristique nec, feugiat tempus justo. Etiam pellentesque, augue vel gravida gravida, ipsum ipsum feugiat urna, ac condimentum arcu turpis nec odio. Nam lacus est, aliquam a tortor ut, ultricies posuere ligula. Integer varius accumsan porttitor. Fusce feugiat ornare semper. Donec sed nunc id neque ullamcorper scelerisque. Integer ultrices sapien in bibendum finibus.</p>'
-	
-	pageTitle = samplePageTitle;
-	if (this.get('pageName')){
-		pageTitle = '<h2>'+this.get('pageName')+'</h2>';
-	}
-
-	pageContent = pageTitle + samplePageContent;
-	this.set('pageContent', pageContent);
+statcPageComponent.state.pageContent = function(){
+	return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur diam sed ipsum hendrerit finibus. Aenean purus leo, tristique at erat a, viverra consequat lacus. Nulla imperdiet convallis dapibus. Vivamus blandit at sapien at feugiat. Morbi volutpat pellentesque ligula. Vestibulum ac iaculis orci. Nunc tincidunt augue eget metus porta, ac vehicula ipsum fermentum. Phasellus ut pellentesque libero, id dapibus augue. Suspendisse id porta velit. Quisque aliquet faucibus pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.';
 };
 
-statcPageComponent.state.message = function(){
-	return this.get('pageContent');
+statcPageComponent.state.pageTitle = function(){
+	return 'Page Title';
 };
 
 //headerComponent
@@ -35,7 +23,7 @@ var mainComponent = FlowComponents.define('main',function(props){
 	this.set('template',props.template)
 });
 
-mainComponent.state.message = function(){
+mainComponent.state.componentName = function(){
 	return this.get('template');
 };
 
